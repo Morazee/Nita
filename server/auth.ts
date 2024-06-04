bc60a528367817202c571f567fc26d5f9c4a9e39
Morazee <100149661+Morazee@users.noreply.github.com>
2024-06-04 23:08:23 +0100

    Update auth.ts
    disable dangerous linking

--- server/auth.ts
+++ server/auth.ts
@@ -69,12 +69,12 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      allowDangerousEmailAccountLinking: true,
+      allowDangerousEmailAccountLinking: false,
     }),
     Github({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      allowDangerousEmailAccountLinking: true,
+      allowDangerousEmailAccountLinking: false,
     }),
     Credentials({
       authorize: async (credentials) => {
