51e7d90dd17b642d8a9ba41d8e4971c98ff0c459
Morazee <100149661+Morazee@users.noreply.github.com>
2024-06-06 14:52:55 +0100

    Update email.ts
    support@nita.store

--- server/actions/email.ts
+++ server/actions/email.ts
@@ -11,9 +11,9 @@ export const sendTwoFactorTokenByEmail = async (
   token: string
 ) => {
   const { data, error } = await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "support@nita.store",
     to: email,
-    subject: "Sproud and Scribble - Your 2 Factor Token",
+    subject: "Nita Store - Your 2 Factor Token",
     html: `<p>Your Confirmation Code: ${token}</p>`,
   })
   if (error) return console.log(error)
@@ -23,9 +23,9 @@ export const sendTwoFactorTokenByEmail = async (
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`
   const { data, error } = await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "support@nita.store",
     to: email,
-    subject: "Sproud and Scribble - Confirmation Email",
+    subject: "Nita Store - Confirmation Email",
     html: `<p>Click to <a href='${confirmLink}'>confirm your email</a></p>`,
   })
   if (error) return console.log(error)
@@ -35,9 +35,9 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-password?token=${token}`
   const { data, error } = await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "support@nita.store",
     to: email,
-    subject: "Sproud and Scribble - Confirmation Email",
+    subject: "Nita Store - Confirmation Email",
     html: `<p>Click here <a href='${confirmLink}'>reset your password</a></p>`,
   })
   if (error) return console.log(error)
